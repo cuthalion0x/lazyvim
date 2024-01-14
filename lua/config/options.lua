ff34acd64f6b7903b7fe601fbaf67f8cde9a6632
cuthalion0x <cuthalion0x@proton.me>
2024-01-14 16:41:07 -0500

    remove relative line numbers - prefer absolute

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.relativenumber = false -- Absolute line numbers
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
 vim.opt.wrap = true -- Enable line wrap
